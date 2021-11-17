840363a4e0ff819aa765e92ead7fffc524ead515
tshanebuckley <42987118+tshanebuckley@users.noreply.github.com>
2021-11-17 12:40:13 -0500

    Update run_heudiconv.py

--- clpipe/run_heudiconv.py
+++ clpipe/run_heudiconv.py
@@ -100,10 +100,10 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_co
     #    run_str = 'singularity run --cleanenv -B {proj_dir},{home_dir} {heudiconv}'.format() #TODO: finish this pip option
     if session_toggle and not longitudinal:
         # --cleanenv
-        heud_string = run_str + ''' -d {dicom_dir} -s {subject} -ss {session} -o {bids_dir} -f {conv_config_file} -b -c dcm2niix'''
+        heud_string = '''singularity run --cleanenv -B {proj_dir},{home_dir} {heudiconv} -d {dicom_dir} -s {subject} -ss {session} -o {bids_dir} -f {conv_config_file} -b -c dcm2niix'''
     else:
         # --cleanenv
-        heud_string = run_str + ''' -d {dicom_dir} -s {subject} -o {bids_dir} -f {conv_config_file} -b -c dcm2niix'''
+        heud_string = '''singularity run --cleanenv -B {proj_dir},{home_dir} {heudiconv} -d {dicom_dir} -s {subject} -o {bids_dir} -f {conv_config_file} -b -c dcm2niix'''
 
     if overwrite:
         heud_string = heud_string + " --overwrite"
@@ -111,9 +111,9 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_co
     if asc_num:
         heud_string = heud_string + " -g accession_number"
 
-    heud_string.format(heudiconv = config.config['DICOMToBIDSOptions']['HeudiconvPath'])
-    formatted_dir = config.config['DICOMToBIDSOptions']['HeudiconvFormatString']
-    dicom_dir = config.config['DICOMToBIDSOptions']['HeudiconvFormatString']
+    #heud_string.format(heudiconv = config.config['DICOMToBIDSOptions']['HeudiconvPath'])
+    #formatted_dir = config.config['DICOMToBIDSOptions']['HeudiconvFormatString']
+    #dicom_dir = config.config['DICOMToBIDSOptions']['HeudiconvFormatString']
 
     batch_manager = BatchManager(config.config['BatchConfig'], config.config['DICOMToBIDSOptions']['LogDirectory'])
     batch_manager.createsubmissionhead()
